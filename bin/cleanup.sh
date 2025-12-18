5b7842e25b18ac13f3d4a2a24d4cb04ec3b52827
kumar7-rajat <kumar7.rajat@users.noreply.github.com>
2025-12-19 00:51:32 +0530

    Updating cleanup.sh

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -1,11 +1,11 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 # This script used to check for unused resources and calculate the estimated cost that can be saved by cleaning them up.
 
-set -eou pipefail
+set -euo pipefail
 
 #Sourcing config.env file 
-source ../config/config.env
+source "../config/config.env"
 
 
 log () { 
@@ -15,34 +15,46 @@ log () {
 log "Verifying the AWS Account"
 ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 
-if [ "$ALLOWED_ACCOUNT_IDS"== "$ACCOUNT_ID"]
+if [ "$ALLOWED_ACCOUNT_IDS" == "$ACCOUNT_ID" ]
     then
         log "Account Found: $ACCOUNT_ID"
 else
     log "Unauthorized AWS account found"
     exit 1
+fi
 
-RUN_ID = $(date +'%Y-%m-%d %H:%M:%S')
-WORDKDIR = "/tmp/cloudResource/$RUNID"
+RUN_ID=$( date +'%Y-%m-%d %H:%M:%S' )
+WORKDIR="/tmp/cloudResource/$RUN_ID"
 
 log "Creating Working Directory"
-mkdir -p "$WORDKDIR"
+mkdir -p "$WORKDIR"
 
 log "Collecting AWS Regions information"
-$REGIONS=$(aws ec2 describe-regions --query "Regions[].RegionName" --output text)
+REGIONS=$( aws ec2 describe-regions --query "Regions[].RegionName" --output text )
 log "Regions: $REGIONS"
 echo "$REGIONS" > regions.txt
 
 #Creating EC2 working directory
-EC2_DIR="$WORDKDIR/ec2"
-mkdir -p $EC2_DIR
+EC2_DIR="$WORKDIR/ec2"
+mkdir -p "$EC2_DIR"
 
 log "Starting stopped EC2 instances across regions"
 
+count=1
 for region in $REGIONS
     do
-        aws ec2 describe-instances --region $region --filter Name=instance-state-name,Values=stopped --output json > $EC2_DIR/stopped-instance-$region || {
-            log " WARNING: Failed to scan region: $region"
-            continue
-        }
-    done
+        temp_file="$EC2_DIR/tmp.json"
+        aws ec2 describe-instances --region $region --filter Name=instance-state-name,Values=stopped --output json > "$temp_file"
+        INSTANCE_COUNT=$( jq '[.Reservations[].Instances[] | select(.State.Name=="stopped")] | length' "$temp_file" )
+        if [ $INSTANCE_COUNT -gt 0 ]
+            then
+                log "$count Stopped instance found in $region"
+                mv "$temp_file" "$EC2_DIR/stopped-$region.json"
+                count=$(( $count+1 ))
+            else
+                log "No Stopped instance found in $region"
+                rm -rf $temp_file
+        fi
+done
+
+log "Script Exiting..."
